cdadd45a16ad2ca41ddfd56d65ed54908462e68f
Amit Kumar <amit_kumar96@outlook.com>
2019-03-19 13:41:14 +0530

    Update application.py

--- application.py
+++ application.py
@@ -110,8 +110,8 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <h1>Amit</h1>
+    <p>Testing AWS CI and CD.</p>
   </div>
   
   <div class="linksColumn"> 
